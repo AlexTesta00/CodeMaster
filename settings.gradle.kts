825c2ee6a78777bd7f230971b8696a11c5e886e1
Alex Testa <alex.testa@studio.unibo.it>
2025-02-08 11:49:44 +0100

    build(gradle): Add conventional commit plugin for enforcing commit syntax

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,3 +7,13 @@
  */
 
 rootProject.name = "CodeMaster"
+
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.20"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    createHooks(true)
+}
