578db53a909516c20ec98a8c8b2f814644b841bd
Alex Testa <alex.testa@studio.unibo.it>
2025-02-14 15:17:10 +0100

    refactor(user-repository): refactor user-repository-impl and change the return type of findUserByNickname findUserByEmail function

--- authentication-service/src/main/nodejs/codemaster/servicies/authentication/infrastructure/user-repository.ts
+++ authentication-service/src/main/nodejs/codemaster/servicies/authentication/infrastructure/user-repository.ts
@@ -1,10 +1,9 @@
 import {User} from "../domain/user";
-import { Salt } from "../domain/user-salt";
 
 export interface UserRepository {
     save(user: User): Promise<void>;
-    findUserByNickname(nickname: string): Promise<[User, Salt] | null>;
-    findUserByEmail(email: string): Promise<[User, Salt] | null>;
+    findUserByNickname(nickname: string): Promise< User>;
+    findUserByEmail(email: string): Promise<User>;
     updateUserEmail(nickname: string, newEmail: string): Promise<void>;
     updateUserPassword(nickname: string, newPassword: string): Promise<void>;
     deleteUser(nickname: string): Promise<void>;
