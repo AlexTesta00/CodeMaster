7c7f9985be9c0ef57ad1bcb63f06474a95210f28
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2025-02-14 19:51:39 +0100

    feat(codequest-test): add codequest-factory test

--- codequest-services/src/test/codequest-factory.test.ts
+++ codequest-services/src/test/codequest-factory.test.ts
@@ -1,1 +1,32 @@
+import { error } from "console";
+import { CodeQuest } from "../main/nodejs/codemaster/servicies/codequest/domain/codequest";
+import { CodeQuestFactory } from "../main/nodejs/codemaster/servicies/codequest/domain/codequest-factory";
 
+
+describe('TestCodeQuestFactory', () => {
+
+    const author = 'example';
+    const problem = 'Realise a calculator';
+    const title = 'Calculator';
+    const id = 'test\\1234'
+
+    it('should create codequest with correct values', async () => {
+        const codequest = CodeQuestFactory.createCodeQuest(id, title, author, problem);
+        expect(codequest.author).toBe(author);
+        expect(codequest.problem).toBe(problem);
+        expect(codequest.title).toBe(title);
+    }, 10000);
+
+    it('should throw error when create codequest if problem is empty', async () => {
+        expect(() => {CodeQuestFactory.createCodeQuest(id, title, author, "")}).toThrow(new Error('Invalid nickname: this user doesn\'t exist'));
+    }, 10000);
+
+    it('should throw error when create codequest if author\'s nickname is empty', async () => {
+        expect(() => {CodeQuestFactory.createCodeQuest(id, title, "", problem)}).toThrow(new Error('Invalid problem: problem\'s body cannot be empty'));
+    }, 10000);
+
+    it('should throw error when create codequest if title is empty', async () => {
+        expect(() => {CodeQuestFactory.createCodeQuest(id, "", author, problem)}).toThrow(new Error('Invalid title: title\'s body cannot be empty'));
+    }, 10000);
+
+});
