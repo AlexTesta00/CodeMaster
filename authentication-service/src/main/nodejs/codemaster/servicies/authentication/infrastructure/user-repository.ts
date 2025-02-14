0e09dd03675ba9f86120bed39f629bb588849d1e
Alex Testa <alex.testa@studio.unibo.it>
2025-02-14 11:43:39 +0100

    feat(user-repository): Add method to updateUserEmail, updateUserPassword and delete user

--- authentication-service/src/main/nodejs/codemaster/servicies/authentication/infrastructure/user-repository.ts
+++ authentication-service/src/main/nodejs/codemaster/servicies/authentication/infrastructure/user-repository.ts
@@ -5,4 +5,7 @@ export interface UserRepository {
     save(user: User): Promise<void>;
     findUserByNickname(nickname: string): Promise<[User, Salt] | null>;
     findUserByEmail(email: string): Promise<[User, Salt] | null>;
+    updateUserEmail(nickname: string, newEmail: string): Promise<void>;
+    updateUserPassword(nickname: string, newPassword: string): Promise<void>;
+    deleteUser(nickname: string): Promise<void>;
 }
