2b086e114b69c6e27fa85a0e9311a08c746f2fb8
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2025-02-16 17:51:16 +0100

    refactor(codequest-model): now author is not a key nor a unique value

--- codequest-services/src/main/nodejs/codemaster/servicies/codequest/domain/codequest-model.ts
+++ codequest-services/src/main/nodejs/codemaster/servicies/codequest/domain/codequest-model.ts
@@ -1,11 +1,11 @@
 import mongoose, {Schema} from 'mongoose';
 
 const codequestSchema = new Schema({
-    "id": {type:'string', required: true, unique: true, key: true},
-    "author": {type: 'string', required: true, unique: true, key: true},
-    "problem": {type: 'string', required: true},
-    "timestamp": {type: Date, required: true, unique: true},
-    "title": {type: 'string', required: true}
+    id: {type:'string', required: true, unique: true, key: true},
+    author: {type: 'string', required: true},
+    problem: {type: 'string', required: true},
+    timestamp: {type: Date, required: true, unique: true},
+    title: {type: 'string', required: true}
 });
 
 export const CodeQuestModel = mongoose.model('Codequest', codequestSchema);
