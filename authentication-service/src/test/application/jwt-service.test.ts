77aa8d8f320d68fe1aef971ee6b3676134e9155f
Alex Testa <alex.testa@studio.unibo.it>
2025-02-17 14:58:14 +0100

    test(jwt-service): add test for jwt service

---
+++ authentication-service/src/test/application/jwt-service.test.ts
@@ -0,0 +1,66 @@
+import jwt, {JwtPayload} from 'jsonwebtoken';
+import { JWTService } from "../../main/nodejs/codemaster/servicies/authentication/application/jwt-service";
+import { JWTServiceImpl} from "../../main/nodejs/codemaster/servicies/authentication/application/jwt-service-impl";
+import {
+    AuthenticationServiceError
+} from "../../main/nodejs/codemaster/servicies/authentication/application/authentication-service";
+
+describe('Test JWT Service', () => {
+    const timeout: number = 10000;
+    const nickname: string = "example";
+    const email: string = 'test@test.com';
+    const password: string = 'Test1234!';
+    let jwtService: JWTService;
+    let accesstoken: string;
+    let refreshToken: string;
+
+    beforeAll(() => {
+        jwtService = new JWTServiceImpl();
+        accesstoken = jwtService.generateAccessToken(nickname, email);
+        refreshToken = jwtService.generateRefreshToken(nickname, email);
+    });
+
+    describe('Test access token', () => {
+        it('should generate a valid access token', () => {
+            expect(accesstoken).toBeDefined();
+            expect(typeof accesstoken).toBe('string');
+            const decoded = jwt.verify(accesstoken, 'access') as JwtPayload;
+            expect(decoded.nickname).toBe(nickname);
+            expect(decoded.email).toBe(email);
+        });
+
+        it('should verify a valid access token', () => {
+            const decoded = jwtService.verifyAccessToken(accesstoken);
+            expect(decoded).toBeDefined();
+            expect((decoded as JwtPayload).nickname).toBe(nickname);
+            expect((decoded as JwtPayload).email).toBe(email);
+        });
+
+        it('should reject if access token is invalid', () => {
+            const invalidToken = "invalid.invalid";
+            expect(() => jwtService.verifyAccessToken(invalidToken)).toThrow(AuthenticationServiceError.InvalidAccessToken);
+        });
+    });
+
+    describe("Test refresh token", () => {
+        it('should generate a valid refresh token', () => {
+            expect(refreshToken).toBeDefined();
+            expect(typeof refreshToken).toBe('string');
+            const decoded = jwt.verify(refreshToken, 'refresh') as JwtPayload;
+            expect(decoded.nickname).toBe(nickname);
+            expect(decoded.email).toBe(email);
+        });
+
+        it('should verify a valid refresh token', () => {
+            const decoded = jwtService.verifyRefreshToken(refreshToken);
+            expect(decoded).toBeDefined();
+            expect((decoded as JwtPayload).nickname).toBe(nickname);
+            expect((decoded as JwtPayload).email).toBe(email);
+        });
+
+        it('should reject if refresh token is invalid', () => {
+            const invalidToken = "invalid.invalid";
+            expect(() => jwtService.verifyRefreshToken(invalidToken)).toThrow(AuthenticationServiceError.InvalidRefreshToken);
+        });
+    });
+})
