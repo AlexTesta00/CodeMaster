a08783f98725abbbfd3113e66df9220ea57f3bd8
Alex Testa <alex.testa@studio.unibo.it>
2025-02-18 14:49:35 +0100

    refactor(user-factory): now class use validator to check nickname, email, passowrd fairness

--- authentication-service/src/main/nodejs/codemaster/servicies/authentication/domain/user-factory-impl.ts
+++ authentication-service/src/main/nodejs/codemaster/servicies/authentication/domain/user-factory-impl.ts
@@ -1,24 +1,19 @@
 import { User } from './user';
 import { UserId } from './user-id';
 import { UserFactory, UserFactoryError } from './user-factory';
+import {Validator} from "./validator";
 
 export class UserFactoryImpl implements UserFactory {
-
-    private NICKNAME_REGEX = /^[a-zA-Z0-9_]{3,10}$/; //Only letter, number and underscore. Min 3, max 10 characters
-    private EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
-    private PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/
-
     createUser(nickname: string, email: string, password: string): User {
-        if(!this.NICKNAME_REGEX.test(nickname)){
+        if(!Validator.isValidNickname(nickname)){
             throw new UserFactoryError.InvalidNickname('Invalid nickname format, only letter, number and underscore. Min 3, max 10 characters');
         }
 
-        if(!this.EMAIL_REGEX.test(email)){
+        if(!Validator.isValidEmail(email)){
             throw new UserFactoryError.InvalidEmail('Invalid email format');
         }
 
-        if(!this.PASSWORD_REGEX.test(password)){
+        if(!Validator.isValidPassword(password)){
             throw new UserFactoryError.InvalidPassword('Invalid password format, at least 8 characters, one uppercase letter, one number and one special character');
         }
 
