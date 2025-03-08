9f46433580c123b35c2200c287837c0d966cb3b0
Alex Testa <alex.testa@studio.unibo.it>
2025-03-08 15:37:59 +0100

    build(eslint): setup eslint to imptrove code quality

--- authentication-service/eslint.config.mjs
+++ authentication-service/eslint.config.mjs
@@ -5,8 +5,13 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.ts"],
+    ignores: ["tests/**", "__tests__/**", "dist/**"],
+  },
+  {languageOptions: {
+    globals: globals.browser
+  }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
