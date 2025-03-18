026ccc3ea6bba80ebb09812eff016a74219cd044
sbrambalds <riccardo.rambaldi4@studio.unibo.it>
2025-03-18 22:04:30 +0100

    refactor(language): change error message in language factory

--- codequest-service/src/main/nodejs/codemaster/servicies/codequest/domain/language/language-factory.ts
+++ codequest-service/src/main/nodejs/codemaster/servicies/codequest/domain/language/language-factory.ts
@@ -3,11 +3,11 @@ import { Language } from "./language";
 export class LanguageFactory{
     static createLanguage(name: String, versions: String[]): Language {
         if(!name || name == "") {
-            throw new LanguageError.InvalidName('Can\'t find \"' + name + '\" code language in the database')
+            throw new LanguageError.InvalidName('Invalid code language name: \"' + name + '\"')
         } 
 
         if(!versions || versions.length == 0) {
-            throw new LanguageError.InvalidVersion('This version of \"' + name + '\" is not available')
+            throw new LanguageError.InvalidVersion('Invalid version of \"' + name + '\" language')
         }
 
         return new Language(name, versions);
