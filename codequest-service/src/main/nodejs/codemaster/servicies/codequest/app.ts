b7532eaedf3adf97932525c2d8c64b47aea63c77
sbrambalds <riccardo.rambaldi4@studio.unibo.it>
2025-03-19 13:27:46 +0100

    feat(codequest-service): add erro handler in express server

--- codequest-service/src/main/nodejs/codemaster/servicies/codequest/app.ts
+++ codequest-service/src/main/nodejs/codemaster/servicies/codequest/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./interfaces/routers/codequest-routes";
 import { connectToDatabase } from "./infrastructure/db-connection";
 import * as dotenv from 'dotenv'
+import { errorHandler } from "./interfaces/error-handler";
 
 dotenv.config();
 
@@ -11,10 +12,9 @@ const port = process.env.PORT!
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 //app.use(helmet())
-//app.use(cookieParser())
 
 app.use('/codequests', router)
-//app.use(errorHandler)
+app.use(errorHandler)
 
 app.listen(port, async () => {
   console.log(`Server is running on port ${port}`)
