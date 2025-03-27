61448ee1c42ecfe1a0dcbeab4bf9497225f23772
Alex Testa <alex.testa@studio.unibo.it>
2025-03-27 14:00:30 +0100

    refactor(user-repository): refactor user-repository and it's look more functional

--- user-service/src/main/nodejs/codemaster/servicies/user/infrastructure/user-repository.ts
+++ user-service/src/main/nodejs/codemaster/servicies/user/infrastructure/user-repository.ts
@@ -3,46 +3,89 @@ import { User, UserId } from '../domain/user'
 import { createLevel } from '../domain/level-factory'
 import { Either, left, right } from 'fp-ts/Either'
 import { createAdvancedUser, createDefaultUser } from '../domain/user-factory'
-import { some, none, isSome } from 'fp-ts/Option'
+import { none, getOrElse, map, flatMap, toNullable, fromNullable } from 'fp-ts/Option'
 import { UserManagerModel } from './schema'
 import { UnknownError, UserNotFound } from './repository-error'
-import { ProfilePicture } from '../domain/profile-picture'
-import { Trophy } from '../domain/trophy'
 import { Language } from '../domain/language'
-import { CV } from '../domain/cv'
+import { pipe } from 'fp-ts/function'
+import { createTrophy } from '../domain/trophy-factory'
+
+const DEFAULT_BIO_VALUE = ''
+const DEFAULT_PROFILE_PICTURE_VALUE = { url: '', alt: none }
+const DEFAULT_LANGUAGES_VALUE: Iterable<Language> = []
+const DEFAULT_CV_VALUE = { url: '' }
+const DEFAULT_TROPHIES_VALUE: Array<{
+  title: string
+  description: string
+  url: string
+  xp: number
+}> = []
+
+interface UserDocument {
+  profilePicture: {
+    url: string
+    alt?: string
+  }
+  languages: Array<{ name: string }>
+  trophies: Array<{
+    title: string
+    description: string
+    url: string
+    xp: number
+  }>
+  level: {
+    grade: number
+    title: string
+    xp: number
+  }
+  userInfo: {
+    nickname: string
+    bio?: string
+  }
+  cv: { url: string }
+}
 
 export const toUserManagerModel = (userManager: UserManager) => {
-  const bio: string = isSome(userManager.userInfo.bio)
-    ? userManager.userInfo.bio.value
-    : ''
-  const profilePicture: ProfilePicture = isSome(userManager.profilePicture)
-    ? userManager.profilePicture.value
-    : { url: '', alt: none }
-  const languages: Iterable<Language> = isSome(userManager.languages)
-    ? userManager.languages.value
-    : []
-  const cv: CV = isSome(userManager.cv) ? userManager.cv.value : { url: '' }
-  const trophies: Iterable<Trophy> = isSome(userManager.trophies)
-    ? userManager.trophies.value
-    : []
-  const mapTrophies = Array.from(trophies).map((trophy: Trophy) => ({
-    title: trophy.title.value,
-    description: trophy.description,
-    url: trophy.url,
-    xp: trophy.xp,
-  }))
   return new UserManagerModel({
     userInfo: {
       nickname: userManager.userInfo.nickname.value,
-      bio: bio,
+      bio: pipe(
+        userManager.userInfo.bio,
+        getOrElse(() => DEFAULT_BIO_VALUE)
+      ),
     },
     profilePicture: {
-      url: profilePicture.url,
-      alt: isSome(profilePicture.alt) ? profilePicture.alt : null,
+      url: pipe(
+        userManager.profilePicture,
+        map((p) => p.url),
+        getOrElse(() => DEFAULT_PROFILE_PICTURE_VALUE.url)
+      ),
+      alt: pipe(
+        userManager.profilePicture,
+        flatMap((p) => p.alt),
+        toNullable
+      ),
     },
-    languages: languages,
-    cv: cv,
-    trophies: mapTrophies,
+    languages: pipe(
+      userManager.languages,
+      getOrElse(() => DEFAULT_LANGUAGES_VALUE)
+    ),
+    cv: pipe(
+      userManager.cv,
+      getOrElse(() => DEFAULT_CV_VALUE)
+    ),
+    trophies: pipe(
+      userManager.trophies,
+      map((iterable) =>
+        Array.from(iterable).map((trophy) => ({
+          title: trophy.title.value,
+          description: trophy.description,
+          url: trophy.url,
+          xp: trophy.xp,
+        }))
+      ),
+      getOrElse(() => DEFAULT_TROPHIES_VALUE)
+    ),
     level: {
       grade: userManager.level.grade.value,
       title: userManager.level.title,
@@ -51,37 +94,32 @@ export const toUserManagerModel = (userManager: UserManager) => {
   })
 }
 
-//TODO: Fix any type
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const toUserManager = (userDocument: any): UserManager => {
-  const alt = userDocument.profilePicture.alt
-    ? some(userDocument.profilePicture.alt)
-    : none
-  const profilePicture = { url: userDocument.profilePicture.url, alt: alt }
-  const languages = userDocument.languages.map((language: Language) => ({
-    name: language.name,
-  }))
-  const trophies = userDocument.trophies.map((trophy: Trophy) => ({
-    title: { value: trophy.title },
-    description: trophy.description,
-    url: trophy.url,
-    xp: trophy.xp,
-  }))
-  const level = createLevel(
-    userDocument.level.grade,
-    userDocument.level.title,
-    userDocument.level.xp
+export const toUserManager = (userDocument: UserDocument): UserManager =>
+  pipe(
+    {
+      nickname: userDocument.userInfo.nickname,
+      bio: pipe(
+        fromNullable(userDocument.userInfo.bio),
+        getOrElse(() => '')
+      ),
+      profilePicture: {
+        url: userDocument.profilePicture.url,
+        alt: fromNullable(userDocument.profilePicture.alt),
+      },
+      languages: userDocument.languages,
+      cv: userDocument.cv,
+      trophies: userDocument.trophies.map((trophy) =>
+        createTrophy(trophy.title, trophy.description, trophy.url, trophy.xp)
+      ),
+      level: createLevel(
+        userDocument.level.grade,
+        userDocument.level.title,
+        userDocument.level.xp
+      ),
+    },
+    ({ nickname, bio, profilePicture, languages, cv, trophies, level }) =>
+      createAdvancedUser(nickname, bio, profilePicture, languages, cv, trophies, level)
   )
-  return createAdvancedUser(
-    userDocument.userInfo.nickname,
-    userDocument.userInfo.bio,
-    profilePicture,
-    languages,
-    userDocument.cv,
-    trophies,
-    level
-  )
-}
 
 export const saveDefaultUser = async (
   userInfo: User
@@ -126,31 +164,17 @@ export const updateUserInfo = async (
   newUserInfo: UserManager
 ): Promise<Either<Error, UserManager>> => {
   try {
-    const userBio = isSome(newUserInfo.userInfo.bio) ? newUserInfo.userInfo.bio.value : ''
-    const profilePicture = isSome(newUserInfo.profilePicture)
-      ? newUserInfo.profilePicture.value
-      : { url: '', alt: none }
-    const languages = isSome(newUserInfo.languages) ? newUserInfo.languages.value : []
-    const cv = isSome(newUserInfo.cv) ? newUserInfo.cv.value : { url: '' }
-    const trophies = isSome(newUserInfo.trophies) ? newUserInfo.trophies.value : []
-    const allTrophies = Array.from(trophies).map((trophy: Trophy) => ({
-      title: trophy.title.value,
-      description: trophy.description,
-      url: trophy.url,
-      xp: trophy.xp,
-    }))
+    const userManagerModel = toUserManagerModel(newUserInfo)
     const userDocument = await UserManagerModel.findOneAndUpdate(
       { 'userInfo.nickname': nickname.value },
       {
         $set: {
-          'userInfo.bio': userBio,
-          'profilePicture.url': profilePicture.url,
-          'profilePicture.alt': isSome(profilePicture.alt)
-            ? profilePicture.alt.value
-            : null,
-          languages: languages,
-          cv: cv,
-          trophies: allTrophies,
+          'userInfo.bio': userManagerModel.userInfo.bio,
+          'profilePicture.url': userManagerModel.profilePicture.url,
+          'profilePicture.alt': userManagerModel.profilePicture.alt,
+          languages: userManagerModel.languages,
+          cv: userManagerModel.cv,
+          trophies: userManagerModel.trophies,
           level: {
             grade: newUserInfo.level.grade.value,
             title: newUserInfo.level.title,
