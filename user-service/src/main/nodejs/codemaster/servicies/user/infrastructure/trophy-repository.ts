b3d018b8465111853da118614d160f37c6cf62f6
Alex Testa <alex.testa@studio.unibo.it>
2025-03-28 16:09:07 +0100

    refactor(trophy-repository): move toTrophyModel method into conversion.ts, delete try/catch and use tryCatch()

--- user-service/src/main/nodejs/codemaster/servicies/user/infrastructure/trophy-repository.ts
+++ user-service/src/main/nodejs/codemaster/servicies/user/infrastructure/trophy-repository.ts
@@ -1,52 +1,42 @@
 import { Trophy, TrophyId } from '../domain/trophy'
-import { Either, left, right } from 'fp-ts/Either'
+import { Either } from 'fp-ts/Either'
 import { TrophyNotFound, UnknownError } from './repository-error'
 import { TrophyModel } from './schema'
 import { createTrophy } from '../domain/trophy-factory'
+import { toTrophyModel } from './conversion'
+import { tryCatch } from 'fp-ts/TaskEither'
 
-export const toTrophyModel = (trophy: Trophy) => {
-  return new TrophyModel({
-    title: trophy.title.value,
-    description: trophy.description,
-    url: trophy.url,
-    xp: trophy.xp,
-  })
-}
+export const saveTrophy = async (trophy: Trophy): Promise<Either<Error, Trophy>> =>
+  tryCatch(
+    async () => {
+      await toTrophyModel(trophy).save()
+      return trophy
+    },
+    (error) => (error instanceof Error ? error : new UnknownError())
+  )()
 
-export const saveTrophy = async (trophy: Trophy): Promise<Either<Error, Trophy>> => {
-  try {
-    await toTrophyModel(trophy).save()
-    return right(trophy)
-  } catch (error) {
-    void error
-    return left(error instanceof Error ? error : new UnknownError())
-  }
-}
+export const findTrophy = async (trophyId: TrophyId): Promise<Either<Error, Trophy>> =>
+  tryCatch(
+    async () => {
+      const trophyDocument = await TrophyModel.findOne({ title: trophyId.value }).exec()
+      if (!trophyDocument) throw new TrophyNotFound('Trophy not found')
+      return createTrophy(
+        trophyDocument.title,
+        trophyDocument.description,
+        trophyDocument.url,
+        trophyDocument.xp
+      )
+    },
+    (error) => (error instanceof Error ? error : new UnknownError())
+  )()
 
-export const findTrophy = async (trophyId: TrophyId): Promise<Either<Error, Trophy>> => {
-  try {
-    const trophyDocument = await TrophyModel.findOne({ title: trophyId.value }).exec()
-    if (!trophyDocument) return left(new TrophyNotFound('Trophy not found'))
-    const trophy: Trophy = createTrophy(
-      trophyDocument.title,
-      trophyDocument.description,
-      trophyDocument.url,
-      trophyDocument.xp
-    )
-    return right(trophy)
-  } catch (error) {
-    return left(error instanceof Error ? error : new UnknownError())
-  }
-}
-
-export const deleteTrophy = async (trophyId: TrophyId): Promise<Either<Error, void>> => {
-  try {
-    const trophyDocument = await TrophyModel.findOneAndDelete({
-      title: trophyId.value,
-    }).exec()
-    if (!trophyDocument) return left(new TrophyNotFound('Trophy not found'))
-    return right(undefined)
-  } catch (error) {
-    return left(error instanceof Error ? error : new UnknownError())
-  }
-}
+export const deleteTrophy = async (trophyId: TrophyId): Promise<Either<Error, void>> =>
+  tryCatch(
+    async () => {
+      const trophyDocument = await TrophyModel.findOneAndDelete({
+        title: trophyId.value,
+      }).exec()
+      if (!trophyDocument) throw new TrophyNotFound('Trophy not found')
+    },
+    (error) => (error instanceof Error ? error : new UnknownError())
+  )()
