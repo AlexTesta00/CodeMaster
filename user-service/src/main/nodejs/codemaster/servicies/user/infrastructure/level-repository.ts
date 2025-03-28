27eceb8b71063d94062a6e600d71e33fb7a7ae75
Alex Testa <alex.testa@studio.unibo.it>
2025-03-28 16:19:10 +0100

    refactor(level-repository): move toLevelModel into conversion.ts and replace try/catch with tryCatch() method

--- user-service/src/main/nodejs/codemaster/servicies/user/infrastructure/level-repository.ts
+++ user-service/src/main/nodejs/codemaster/servicies/user/infrastructure/level-repository.ts
@@ -1,52 +1,37 @@
 import { Level, LevelId } from '../domain/level'
 import { LevelModel } from './schema'
-import { Either, left, right } from 'fp-ts/Either'
+import { Either } from 'fp-ts/Either'
 import { LevelNotFound, UnknownError } from './repository-error'
 import { createLevel } from '../domain/level-factory'
+import { toLevelModel } from './conversion'
+import { tryCatch } from 'fp-ts/TaskEither'
 
-export const toLevelModel = (level: Level) => {
-  return new LevelModel({
-    grade: level.grade.value,
-    title: level.title,
-    xp: level.xpLevel,
-  })
-}
+export const saveLevel = async (level: Level): Promise<Either<Error, Level>> =>
+  tryCatch(
+    async () => {
+      await toLevelModel(level).save()
+      return level
+    },
+    (error) => (error instanceof Error ? error : new UnknownError())
+  )()
 
-export const saveLevel = async (level: Level): Promise<Either<Error, Level>> => {
-  try {
-    await toLevelModel(level).save()
-    return right(level)
-  } catch (error) {
-    void error
-    return left(error instanceof Error ? error : new UnknownError())
-  }
-}
+export const findLevel = async (levelId: LevelId): Promise<Either<Error, Level>> =>
+  tryCatch(
+    async () => {
+      const levelDocument = await LevelModel.findOne({ grade: levelId.value }).exec()
+      if (!levelDocument) throw new LevelNotFound('Level not found')
+      return createLevel(levelDocument.grade, levelDocument.title, levelDocument.xp)
+    },
+    (error) => (error instanceof Error ? error : new UnknownError())
+  )()
 
-export const findLevel = async (levelId: LevelId): Promise<Either<Error, Level>> => {
-  try {
-    const levelDocument = await LevelModel.findOne({ grade: levelId.value }).exec()
-    if (!levelDocument) return left(new LevelNotFound('Level not found'))
-    const levelFounded = createLevel(
-      levelDocument.grade,
-      levelDocument.title,
-      levelDocument.xp
-    )
-    return right(levelFounded)
-  } catch (error) {
-    void error
-    return left(error instanceof Error ? error : new UnknownError())
-  }
-}
-
-export const deleteLevel = async (levelId: LevelId): Promise<Either<Error, void>> => {
-  try {
-    const levelDocument = await LevelModel.findOneAndDelete({
-      grade: levelId.value,
-    }).exec()
-    if (!levelDocument) return left(new LevelNotFound('Level not found'))
-    return right(undefined)
-  } catch (error) {
-    void error
-    return left(error instanceof Error ? error : new UnknownError())
-  }
-}
+export const deleteLevel = async (levelId: LevelId): Promise<Either<Error, void>> =>
+  tryCatch(
+    async () => {
+      const levelDocument = await LevelModel.findOneAndDelete({
+        grade: levelId.value,
+      }).exec()
+      if (!levelDocument) throw new LevelNotFound('Level not found')
+    },
+    (error) => (error instanceof Error ? error : new UnknownError())
+  )()
