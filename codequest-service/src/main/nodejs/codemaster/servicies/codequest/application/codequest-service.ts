3ede27ae220f2a2e8084cf9759620c73ad44cb6e
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2025-03-31 16:25:48 +0200

    feat(codequest-service): add codequest service to catch error from both language and codequest repository

---
+++ codequest-service/src/main/nodejs/codemaster/servicies/codequest/application/codequest-service.ts
@@ -0,0 +1,22 @@
+import { CodeQuest } from "../domain/codequest/codequest"
+import { Problem } from "../domain/codequest/problem"
+import { Language } from "../domain/language/language"
+
+export interface CodeQuestService {
+  addCodeQuest(title: String, author: String, problem: Problem, timestamp: Date | null, languages: Language[]): Promise<CodeQuest>
+  getCodeQuests(): Promise<CodeQuest[]>;
+  getCodeQuestsById(questId: String): Promise<CodeQuest>;
+  getCodeQuestsByAuthor(author: String): Promise<CodeQuest[]>;
+  getCodeQuestsByLanguage(languageName: String, versions: String[]): Promise<CodeQuest[]>;
+  updateProblem(questId: String, newProblem: Problem): Promise<void>;
+  updateTitle(questId: String, newTitle: String): Promise<void>;
+  updateLanguages(questId: String, newLanguages: Language[]): Promise<void>;
+  delete(questId: String): Promise<void>;
+}
+
+export class CodeQuestServiceError {
+  static InvalidLanguage = class extends Error {}
+  static InvalidCodeQuestId = class extends Error {}
+  static InvalidLanguageVersions = class extends Error {}
+  static CodeQuestNotFound = class extends Error {}
+}
