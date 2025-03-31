467b7bc4d77df07dd100cb27120b6f3696f90541
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2025-03-31 20:23:25 +0200

    refactor(codequest-model): change type of string properties

--- codequest-service/src/main/nodejs/codemaster/servicies/codequest/infrastructure/codequest/codequest-model.ts
+++ codequest-service/src/main/nodejs/codemaster/servicies/codequest/infrastructure/codequest/codequest-model.ts
@@ -2,23 +2,23 @@ import mongoose, {Schema} from 'mongoose';
 import { languageSchema } from '../language/language-model';
 
 const ExampleSchema = new Schema({
-    input: {type: 'string', required: true},
-    output: {type: 'string', required: true},
-    explanation: 'string'
+    input: {type: String, required: true},
+    output: {type: String, required: true},
+    explanation: {type: String}
 })
 
 const ProblemSchema = new Schema({
-    body: {type: 'string', required: true},
+    body: {type: String, required: true},
     examples: { type: [ExampleSchema], required: true },
-    constraints: ['string']
+    constraints: [String]
 })
 
 export const CodequestSchema = new Schema({
-    questId: {type:'string', required: true, unique: true, key: true},
-    author: {type: 'string', required: true},
+    questId: {type: String, required: true, unique: true, key: true},
+    author: {type: String, required: true},
     problem: {type: ProblemSchema, required: true},
     timestamp: {type: Date, default: Date.now()},
-    title: {type: 'string', required: true},
+    title: {type: String, required: true},
     languages: [languageSchema]
 });
 
