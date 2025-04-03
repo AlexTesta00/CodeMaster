8a005e552f737a188894db95f3a7284d88b9157c
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2025-04-03 19:35:54 +0200

    refactor(codequest-factory): add checks for problem's examples and languages

--- codequest-service/src/main/nodejs/codemaster/servicies/codequest/domain/codequest/codequest-factory.ts
+++ codequest-service/src/main/nodejs/codemaster/servicies/codequest/domain/codequest/codequest-factory.ts
@@ -3,6 +3,7 @@ import { Language } from "../language/language";
 import { Problem } from "./problem";
 
 export class CodeQuestFactory{
+
     static newCodeQuest(id: String, title: String, author: String, problem: Problem, timestamp: Date | null, languages: Language[]): CodeQuest {
         if(!author){
             throw new CodeQuestError.InvalidAuthor('Invalid nickname: this user doesn\'t exist');
@@ -16,8 +17,18 @@ export class CodeQuestFactory{
             throw new CodeQuestError.InvalidProblem('Invalid problem: problem require at least one example');
         }
 
+        for(const example of problem.examples) {
+            if(example.input == '' || example.output == '') {
+                throw new CodeQuestError.InvalidProblem('Invalid problem: invalid examples');
+            }
+        }
+
         if(!title || title==""){
             throw new CodeQuestError.InvalidTitle('Invalid title: title\'s body cannot be empty');
+        }
+
+        if(!languages || languages.length==0) {
+            throw new CodeQuestError.InvalidLanguage('Invalid languages: languages cannot be null')
         }
         
         return new CodeQuest(id, title, author, problem, timestamp, languages);
@@ -25,6 +36,7 @@ export class CodeQuestFactory{
 }
 
 export class CodeQuestError{
+    static InvalidLanguage = class extends Error{};
     static InvalidAuthor = class extends Error{};
     static InvalidProblem = class extends Error{};
     static InvalidTitle= class extends Error{};
