3cd61bfeae6451a16f57bc1e9fc973b13374b9fe
Alex Testa <alex.testa@studio.unibo.it>
2025-04-11 12:58:28 +0200

    refactor(app): now app.ts create only express app, and file server.ts start the server

---
+++ user-service/src/main/nodejs/codemaster/servicies/server.ts
@@ -0,0 +1,34 @@
+import { connectToDatabase } from './user/infrastructure/db-connection'
+import { app } from './app'
+import { tryCatch, chain } from 'fp-ts/TaskEither'
+import { pipe } from 'fp-ts/function'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const port = process.env.PORT! || 3000
+
+const connectToDB = tryCatch(
+  () => connectToDatabase(),
+  (error) => new Error('DB connection error: ' + error)
+)
+
+const startApp = tryCatch(
+  () =>
+    new Promise<void>((resolve) => {
+      app.listen(port, () => {
+        console.log(`Server is running on port ${port}`)
+        resolve()
+      })
+    }),
+  (error) => new Error('Server start error: ' + error)
+)
+
+const startServer = pipe(
+  connectToDB,
+  chain(() => startApp)
+)
+
+startServer().catch((error) => {
+  console.error('Error starting server:', error)
+  process.exit(1)
+})
