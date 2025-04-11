c8b2682e3b0515b24882aa1bac33fc5a3b586c59
Alex Testa <alex.testa@studio.unibo.it>
2025-04-11 14:45:15 +0200

    feat(api): add api for user-service

---
+++ user-service/src/main/nodejs/codemaster/servicies/user/interfaces/level-controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express'
+import { deleteLevel, getAllLevels, registerNewLevel } from '../application/user-service'
+import { isLeft } from 'fp-ts/Either'
+
+export const addLevel = async (req: Request, res: Response): Promise<void> => {
+  const { grade, title, xpLevel } = req.body
+  const result = await registerNewLevel({ value: grade }, title, xpLevel)
+  if (isLeft(result))
+    res.status(404).json({ message: result.left.message, success: false })
+  else
+    res
+      .status(200)
+      .json({ message: 'Levels changed', success: true, level: result.right })
+}
+
+export const removeLevel = async (req: Request, res: Response): Promise<void> => {
+  const { grade } = req.params
+  const result = await deleteLevel({ value: parseInt(grade) })
+  if (isLeft(result))
+    res.status(404).json({ message: result.left.message, success: false })
+  else
+    res
+      .status(200)
+      .json({ message: 'Levels changed', success: true, level: result.right })
+}
+
+export const getLevels = async (req: Request, res: Response): Promise<void> => {
+  const result = await getAllLevels()
+  if (isLeft(result))
+    res.status(404).json({ message: result.left.message, success: false })
+  else
+    res
+      .status(200)
+      .json({ message: 'Levels retrieved', success: true, level: result.right })
+}
