9ede1889d6299ebb49c99df5cde22195bddfd2cf
Alex Testa <alex.testa@studio.unibo.it>
2025-05-01 17:21:56 +0200

    feat(frontend): add login and register page

--- frontend-service/src/App.vue
+++ frontend-service/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import Choice from './pages/Choice.vue';
-
+//import Choice from './pages/Choice.vue';
+import Login from './pages/Login.vue'
 </script>
 
 <template class="bg-background dark:bg-bgdark">
-  <choice class="bg-background dark:bg-bgdark"></choice>
+  <!-- <choice class="bg-background dark:bg-bgdark"></choice> -->
+  <login></login>
 </template>
 
 <style scoped>
