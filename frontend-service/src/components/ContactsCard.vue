b2a42c323ab73058639b62b893f255786ee92158
Alex Testa <alex.testa@studio.unibo.it>
2025-05-04 15:32:38 +0200

    feat(frontend): add dashboard page

---
+++ frontend-service/src/components/ContactsCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Card from './Card.vue'
+import ImageLink from './ImageLink.vue'
+
+export type Link = {
+  url: string,
+  image: string,
+  alt: string
+}
+
+defineProps<{
+  links: [Link],
+  imageUrl: string,
+  name: string,
+  role: string,
+  alt: string
+}>()
+</script>
+
+<template>
+<card class="gap-4">
+  <img :src=imageUrl class="w-32 h-32 rounded-full object-cover" :alt=alt>
+  <h1 class="text-white text-base text-center lg:text-3xl">{{ name }}</h1>
+  <h2 class="text-center text-xs text-gray-500 lg:text-xl">{{ role }}</h2>
+  <div class="flex flex-row justify-center items-center gap-6">
+    <ImageLink v-for="link in links" :url=link.url :image-path=link.image :alt=link.alt></ImageLink>
+  </div>
+</card>
+</template>
+
+<style scoped>
+
+</style>
