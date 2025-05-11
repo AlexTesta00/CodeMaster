d464645f6d4ac62a4259748c07c34073f05d2d47
Alex Testa <alex.testa@studio.unibo.it>
2025-05-11 16:11:48 +0200

    feat(frontend): Add settings page

--- frontend-service/src/components/ButtonWithImage.vue
+++ frontend-service/src/components/ButtonWithImage.vue
@@ -8,7 +8,7 @@ defineProps<{
 
 <template>
   <button
-    class="flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-primary rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 ml-4 lg:ml-80"
+    class="flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-primary rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 ml-4"
   >
     <img
       :src="imageUrl"
