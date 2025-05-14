dff9a6e8b18d6dbd8ea08aa973b636bef5feba5d
Alex Testa <alex.testa@studio.unibo.it>
2025-05-14 14:51:43 +0200

    fix(authentication): fix bug that was causing tests fail

--- authentication-service/src/main/nodejs/codemaster/servicies/authentication/infrastructure/db-connection.ts
+++ authentication-service/src/main/nodejs/codemaster/servicies/authentication/infrastructure/db-connection.ts
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose'
 
+const serverTimeout = 5000
+
 export const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI!) //TODO: Tech Debit
-    console.log(`Successfully connected to ${process.env.MONGO_URI!}`)
+    const mongoURI = process.env.MONGO_URI ?? 'mongodb://localhost:27017/codemaster/test'
+    await mongoose.connect(mongoURI, {
+      serverSelectionTimeoutMS: serverTimeout,
+    })
+    console.log(`Successfully connected to ${mongoURI}`)
   } catch (error) {
     console.log(`Connection to DB failed ${error}`)
     throw new ConnectionError()
