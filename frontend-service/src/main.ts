70804b34fba626e764efbe99b919f405c3e3a01b
Alex Testa <alex.testa@studio.unibo.it>
2025-05-14 10:03:30 +0200

    feat(frontend): add code page, this page can provide a code editor for solve codequests

--- frontend-service/src/main.ts
+++ frontend-service/src/main.ts
@@ -3,6 +3,16 @@ import AOS from 'aos'
 import './style.css'
 import 'aos/dist/aos.css'
 import App from './App.vue'
+import { loader } from '@guolao/vue-monaco-editor'
 
 AOS.init()
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+loader.config({
+    paths: {
+        vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.52.2/min/vs',
+    },
+})
+
+app.mount('#app')
