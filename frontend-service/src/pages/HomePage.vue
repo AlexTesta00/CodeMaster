120aaa66af1b9674ee702dc8361959a0b6884397
Alex Testa <alex.testa@studio.unibo.it>
2025-05-17 16:38:09 +0200

    feat(frontend): add easter egg

--- frontend-service/src/pages/HomePage.vue
+++ frontend-service/src/pages/HomePage.vue
@@ -418,12 +418,17 @@ const isOpen = ref(false)
                         src="/images/riccardo-ceo.jpg"
                         alt="Photo of Riccardo Rambaldi"
                     />
-
-                    <h1
-                        class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white"
+                    <a
+                        href="https://www.youtube.com/watch?v=xvFZjo5PgG0"
+                        target="_blank"
+                        rel="noopener noreferrer"
                     >
-                        Riccardo Rambaldi
-                    </h1>
+                        <h1
+                            class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white"
+                        >
+                            Riccardo Rambaldi
+                        </h1>
+                    </a>
 
                     <p class="mt-2 text-gray-500 capitalize dark:text-gray-300">
                         CEO & Full stack developer
@@ -470,16 +475,22 @@ const isOpen = ref(false)
                     <div class="flex items-center mt-8 -mx-2">
                         <img
                             class="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700"
-                            src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-                            alt=""
+                            src="/images/minecraft-creeper.png"
+                            alt="Miencraft Creeper Easter Egg"
                         />
 
                         <div class="mx-2">
-                            <h1
-                                class="font-semibold text-gray-800 dark:text-white"
+                            <a
+                                href="https://www.youtube.com/watch?v=QbM2qlIprTI"
+                                target="_blank"
+                                rel="noopener noreferrer"
                             >
-                                Marco B.
-                            </h1>
+                                <h1
+                                    class="font-semibold text-gray-800 dark:text-white"
+                                >
+                                    Marco B.
+                                </h1>
+                            </a>
                             <span class="text-sm text-gray-500">Students</span>
                         </div>
                     </div>
@@ -501,7 +512,7 @@ const isOpen = ref(false)
                     <div class="flex items-center mt-8 -mx-2">
                         <img
                             class="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700"
-                            src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                            src="/images/2-testimonial.jpg"
                             alt=""
                         />
 
@@ -534,7 +545,7 @@ const isOpen = ref(false)
                     <div class="flex items-center mt-8 -mx-2">
                         <img
                             class="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700"
-                            src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                            src="/images/3-testimonial.jpg"
                             alt=""
                         />
 
