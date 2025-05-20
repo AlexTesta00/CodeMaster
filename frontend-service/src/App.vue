52476c50f708562d5feae219388ad61466c3a7fd
Alex Testa <alex.testa@studio.unibo.it>
2025-05-20 13:04:26 +0200

    feat(frontend): add router-link and router-view to navigate into pages

--- frontend-service/src/App.vue
+++ frontend-service/src/App.vue
@@ -1,23 +1,5 @@
-<script setup lang="ts">
-//import Choice from './pages/ChoicePage.vue';
-//import HomeDashboard from './pages/DashboardPage.vue'
-//import HomeLogin from './pages/LoginPage.vue'
-//import HomeProfile from './pages/ProfilePage.vue'
-//import HomeSetting from './pages/SettingPage.vue'
-//import CodePage from './pages/CodePage.vue'
-//import ErrorPage from './pages/ErrorPage.vue'
-//import NewCodeQuestPage from './pages/NewCodeQuestPage.vue'
-import HomePage from './pages/HomePage.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template class="bg-background dark:bg-bgdark">
-    <home-page />
-    <!-- <choice class="bg-background dark:bg-bgdark"/>-->
-    <!-- <home-login/> -->
-    <!-- <home-dashboard class="bg-background dark:bg-bgdark" /> -->
-    <!-- <home-profile class="bg-background dark:bg-bgdark" /> -->
-    <!-- <home-setting class="bg-background dark:bg-bgdark" /> -->
-    <!-- <code-page class="bg-background dark:bg-bgdark" /> -->
-    <!-- <error-page class="bg-background dark:bg-bgdark" title="Ops.. seems like you are lost" error-code="404" /> -->
-    <!-- <new-code-quest-page class="bg-background dark:bg-bgdark" /> -->
+  <router-view />
 </template>
