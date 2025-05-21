bb5bbaa5eacefaf00329c030d33798fccf1254cc
Alex Testa <alex.testa@studio.unibo.it>
2025-05-21 17:43:19 +0200

    refactor(authentication): now cookie-handler don't use try catch but use Either structure

--- authentication-service/src/main/nodejs/codemaster/servicies/authentication/interfaces/cookie-handler.ts
+++ authentication-service/src/main/nodejs/codemaster/servicies/authentication/interfaces/cookie-handler.ts
@@ -1,9 +1,7 @@
-import { Request, Response, NextFunction } from 'express'
-import { JWTService } from '../application/jwt-service'
-import { JWTServiceImpl } from '../application/jwt-service-impl'
+import { NextFunction, Request, Response } from 'express'
 import { OK } from './status'
-
-const jwtService: JWTService = new JWTServiceImpl()
+import { verifyAccessToken } from '../application/authentication-service'
+import { isRight } from 'fp-ts/Either'
 
 export const userIsAlreadyAuthenticated = async (
   req: Request,
@@ -11,18 +9,22 @@ export const userIsAlreadyAuthenticated = async (
   next: NextFunction
 ): Promise<void> => {
   const authToken = req.cookies.auth_token
+
   if (!authToken) {
     return next()
   }
 
-  try {
-    const decode = jwtService.verifyAccessToken(authToken)
-    if (!decode) {
-      next()
-    }
-    req.body.user = decode
+  const decode = verifyAccessToken(authToken)
+  if(isRight(decode)){
+    req.body.user = decode.right
     res.status(OK).json({ message: 'User is already authenticated', success: true })
-  } catch (error) {
-    next(error)
+  }else{
+    const error = decode.left
+    if (error.name === 'TokenExpiredError') {
+      res.status(OK).json({ message: 'Token expired', success: false })
+    }
+    if (error.name === 'JsonWebTokenError') {
+      res.status(OK).json({ message: 'Invalid token', success: false })
+    }
   }
 }
