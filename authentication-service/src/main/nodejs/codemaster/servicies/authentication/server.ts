168f94dede197a87301497728ff30b206dccb996
Alex Testa <alex.testa@studio.unibo.it>
2025-05-29 14:23:32 +0200

    feat(authentication): add publisher, when user call register or delete API, the events are publish in exchange

--- authentication-service/src/main/nodejs/codemaster/servicies/authentication/server.ts
+++ authentication-service/src/main/nodejs/codemaster/servicies/authentication/server.ts
@@ -1,5 +1,6 @@
 import { connectToDatabase } from './infrastructure/db-connection'
 import { app } from './app'
+import { connectToRabbit } from './infrastructure/publisher'
 
 const port = Number(process.env.PORT!) || 4004
 const hostname = '0.0.0.0'
@@ -27,6 +28,13 @@ const startServer = async () => {
   await connectToDB()
   await startApp()
 }
+
+connectToRabbit()
+  .catch((err) => {
+    console.log('AuthService: Error connecting to RabbitMQ:', err)
+    process.exit(1)
+  })
+  .then(() => console.log('AuthService:: Connected to RabbitMQ successfully'))
 
 startServer().catch((error) => {
   console.error('Error starting server:', error)
