61bb0ade0f98eeb2b66c367f2924f32996925066
Alex Testa <alex.testa@studio.unibo.it>
2025-05-29 14:17:54 +0200

    build(authentication): add task for create and delete rabbitmq container

--- authentication-service/build.gradle.kts
+++ authentication-service/build.gradle.kts
@@ -1,7 +1,9 @@
 import com.github.gradle.node.npm.task.NpmTask
+import com.bmuschko.gradle.docker.tasks.container.*
 
 plugins {
     id("com.github.node-gradle.node") version "7.1.0"
+    id("com.bmuschko.docker-remote-api") version "9.4.0"
 }
 
 node{
@@ -10,9 +12,48 @@ node{
     download.set(true)
 }
 
-tasks.register<NpmTask>("npmTest"){
+// Task per creare il container RabbitMQ
+val createRabbitMQContainer = tasks.register<DockerCreateContainer>("createRabbitMQContainer") {
+    targetImageId("rabbitmq:3-management")
+    containerName.set("test-rabbitmq")
+    hostConfig.portBindings.set(listOf("5672:5672", "15672:15672"))
+}
+
+// Task per avviare il container
+val startRabbitMQ = tasks.register<DockerStartContainer>("startRabbitMQ") {
+    dependsOn(createRabbitMQContainer)
+    targetContainerId(createRabbitMQContainer.get().containerId)
+}
+
+// Task per fermare RabbitMQ
+val stopRabbitMQ = tasks.register<DockerStopContainer>("stopRabbitMQ") {
+    targetContainerId(createRabbitMQContainer.get().containerId)
+    onlyIf { createRabbitMQContainer.get().containerId != null }
+}
+
+// Task per rimuovere il container
+val removeRabbitMQContainer = tasks.register<DockerRemoveContainer>("removeRabbitMQContainer") {
+    doFirst {
+        println("⏳ Waiting for RabbitMQ to stop...")
+        Thread.sleep(2000)
+    }
+    dependsOn(stopRabbitMQ)
+    targetContainerId(createRabbitMQContainer.get().containerId)
+    force.set(true)
+}
+
+// Task npmTest con RabbitMQ
+tasks.register<NpmTask>("npmTest") {
+    dependsOn("npmInstall", startRabbitMQ)
+    finalizedBy(removeRabbitMQContainer)
+
+    doFirst {
+        println("⏳ Waiting for RabbitMQ...")
+        Thread.sleep(10000)
+    }
+
+    environment.set(mapOf("RABBIT_URL" to "amqp://guest:guest@host.docker.internal"))
     args.set(listOf("run", "test"))
-    dependsOn("npmInstall")
 }
 
 tasks.register<NpmTask>("build"){
