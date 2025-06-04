032f2e7c71c26368af0ce85256e877ff937c8cfe
Alex Testa <alex.testa@studio.unibo.it>
2025-06-04 19:04:50 +0200

    feat(docker): now when start services in production, the user-service and authentication-service db populating

--- user-service/src/main/nodejs/codemaster/servicies/user/interfaces/level-route.ts
+++ user-service/src/main/nodejs/codemaster/servicies/user/interfaces/level-route.ts
@@ -3,7 +3,7 @@ import { addLevel, getLevels, removeLevel } from './level-controller'
 
 const levelRouter: Router = Router()
 
-levelRouter.post('/', addLevel)
+levelRouter.post('/', addLevel) //TODO: FIXME: level create /create not / (root)
 levelRouter.delete('/:grade', removeLevel)
 levelRouter.get('/', getLevels)
 
