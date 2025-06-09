a98ef01776ad4fa872aef30906851e5fa7178686
Alex Testa <alex.testa@studio.unibo.it>
2025-06-09 14:33:24 +0200

    feat(frontend): added Loading page

---
+++ frontend-service/src/pages/Loading.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="h-screen w-full flex flex-col justify-center items-center">
+    <img src="/icons/spin.gif" class="w-16 h-16" alt="Loading">
+  </section>
+</template>
