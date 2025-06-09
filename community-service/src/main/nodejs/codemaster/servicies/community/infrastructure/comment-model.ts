85fa244d0e0d828ca6adb43aafa26f43327d215a
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2025-06-09 16:55:06 +0200

    feat(community-service): add repository tests

--- community-service/src/main/nodejs/codemaster/servicies/community/infrastructure/comment-model.ts
+++ community-service/src/main/nodejs/codemaster/servicies/community/infrastructure/comment-model.ts
@@ -2,9 +2,10 @@ import mongoose, { Schema } from "mongoose"
 
 export const CommentSchema = new Schema({
   id: { type: 'string', required: true, unique: true, key: true },
-  questId: { type: 'string', required: true, unique: true, key: true },
+  questId: { type: 'string', required: true },
   author: { type: 'string', required: true },
-  timestamp: { type: Date }
+  content: { type: 'string', required: true },
+  timestamp: { type: Date, required: true }
 })
 
-export const CodeQuestModel = mongoose.model('Comment', CommentSchema)
+export const CommentModel = mongoose.model('Comment', CommentSchema)
--- community-service/src/main/nodejs/codemaster/servicies/community/infrastructure/comment-repository-impl.ts
+++ community-service/src/main/nodejs/codemaster/servicies/community/infrastructure/comment-repository-impl.ts
@@ -1,21 +1,47 @@
 import { CommentRepository } from './comment-repository'
 import { CommentId } from '../domain/comment-id'
 import { Comment } from '../domain/comment'
-import { Promise } from 'mongoose'
+import { CommentModel } from './comment-model'
+import { CommentFactory } from '../domain/comment-factory'
+import { DeleteResult } from 'mongoose'
 
 export class CommentRepositoryImpl implements CommentRepository {
 
+  async save(comment: Comment): Promise<Comment> {
 
-  getCommentById(id: CommentId): Promise<Comment> {
-    return Promise.resolve(undefined)
+    return await new CommentModel({
+      id: comment.id,
+      questId: comment.questId,
+      author: comment.author,
+      content: comment.content,
+      timestamp: comment.timestamp
+    }).save()
   }
 
-  getCommentsByCodequest(questId: string): Promise<Comment[]> {
-    return Promise.resolve([])
+  async getCommentById(id: CommentId): Promise<Comment> {
+    return CommentModel.findOne({ id }).orFail()
   }
 
-  save(comment: Comment): Promise<Comment> {
-    return Promise.resolve(undefined)
+  async getCommentsByCodequest(questId: string): Promise<Comment[]> {
+    const comments = await CommentModel.find({ questId: questId })
+    return comments.map((comment) => CommentFactory.newComment(comment.content, comment.questId, comment.author, comment.timestamp, comment.id))
+  }
+
+  async updateContent(id: CommentId, newContent: string): Promise<Comment> {
+    await CommentModel.findOneAndUpdate({ id }, { content: newContent }).orFail()
+    return CommentModel.findOne({ id }).orFail()
+  }
+
+  async deleteComment(id: CommentId): Promise<Comment> {
+    return CommentModel.findOneAndDelete({ id }).orFail()
+  }
+
+  async deleteCommentsByAuthor(user: string): Promise<DeleteResult> {
+    return CommentModel.deleteMany({ author: user }).orFail()
+  }
+
+  async deleteCommentsByCodequest(questId: string): Promise<DeleteResult> {
+    return CommentModel.deleteMany({ questId: questId }).orFail()
   }
 
 }
