d81514839fa6815ee840eafd3b9c530628c54f91
Alex Testa <alex.testa@studio.unibo.it>
2025-06-11 14:06:04 +0200

    feat(frontend): added possibility to chancge profile bio,cv and favorite languages

--- frontend-service/src/utils/api.ts
+++ frontend-service/src/utils/api.ts
@@ -1,24 +1,10 @@
 import axios from 'axios'
+import type { AuthenticationResponse, UserManagerResponse } from './interface.ts'
 
-const API_URL = 'http://localhost/api/v1/authentication/'
+const AUTHENTICATION_URL = 'http://localhost/api/v1/authentication/'
+const USER_URL = 'http://localhost/api/v1/users/'
 // Enable sending cookies with requests
 axios.defaults.withCredentials = true
-
-export interface AuthenticationResponse {
-  message: string
-  success: boolean
-  token?: string
-  user?: {
-    info: {
-      nickname: string
-      email: string
-      password: string
-      role: 'admin' | 'user'
-    }
-    banned: boolean
-    refreshToken: string
-  }
-}
 
 export const registerNewUser = async (
   nickname: string,
@@ -26,7 +12,7 @@ export const registerNewUser = async (
   password: string,
   role: string = 'user'
 ): Promise<AuthenticationResponse> => {
-  const response = await axios.post(`${API_URL}register`, {
+  const response = await axios.post(`${AUTHENTICATION_URL}register`, {
     nickname,
     email,
     password,
@@ -39,9 +25,69 @@ export const loginUser = async (
   nickname: string,
   password: string
 ): Promise<AuthenticationResponse> => {
-  const response = await axios.post(`${API_URL}login`, {
+  const response = await axios.post(`${AUTHENTICATION_URL}login`, {
     nickname,
     password,
+  })
+  return response.data
+}
+
+export const logoutUser = async (
+  nickname: string
+): Promise<void> => {
+  const response = await axios.post(`${AUTHENTICATION_URL}logout`, {
+    nickname,
+  })
+  return response.data
+}
+
+export const getUserByNickname = async (
+  nickname: string
+): Promise<UserManagerResponse> => {
+  const response = await axios.get(`${USER_URL}${nickname}`)
+  return response.data
+}
+
+export const updateProfilePicture = async (
+  nickname: string,
+  newProfilePicture: { url: string, alt: string}
+): Promise<UserManagerResponse> => {
+  const response = await axios.put(`${USER_URL}profile-picture`, {
+    nickname,
+    newProfilePicture,
+  })
+  return response.data
+}
+
+export const updateCV = async (
+  nickname: string,
+  newCV: { url: string }
+): Promise<UserManagerResponse> => {
+  const response = await axios.put(`${USER_URL}cv`, {
+    nickname,
+    newCV,
+  })
+  return response.data
+}
+
+export const updateBio = async (
+  nickname: string,
+  newBio: string
+): Promise<UserManagerResponse> => {
+  const response = await axios.put(`${USER_URL}bio`, {
+    nickname,
+    newBio,
+  })
+  return response.data
+}
+
+export const updateLanguages = async (
+  nickname: string,
+  newLanguages: {name: string}[]
+): Promise<UserManagerResponse> => {
+  const response = await axios.put(`${USER_URL}languages`, {
+    nickname,
+    newLanguages,
   })
   return response.data
 }
---
+++ frontend-service/src/utils/interface.ts
@@ -0,0 +1,75 @@
+import type { Option } from 'fp-ts/Option'
+
+export interface AuthenticationResponse {
+  message: string
+  success: boolean
+  token?: string
+  user?: {
+    info: {
+      nickname: string
+      email: string
+      password: string
+      role: 'admin' | 'user'
+    }
+    banned: boolean
+    refreshToken: string
+  }
+}
+
+export interface UserId {
+  value: string
+}
+
+export interface User {
+  nickname: UserId
+  bio: Option<string>
+}
+
+export interface ProfilePicture {
+  url: string
+  alt: Option<string>
+}
+
+export interface Language {
+  name: string
+}
+
+export interface CV {
+  url: string
+}
+
+export interface TrophyId {
+  value: string
+}
+
+export interface Trophy {
+  title: TrophyId
+  description: string
+  url: string
+  xp: number
+}
+
+export interface LevelId {
+  value: number
+}
+
+export interface Level {
+  grade: LevelId
+  title: string
+  xpLevel: number
+}
+
+export interface UserManager {
+  userInfo: User
+  profilePicture: Option<ProfilePicture>
+  languages: Option<Iterable<Language>>
+  cv: Option<CV>
+  trophies: Option<Iterable<Trophy>>
+  level: Level
+}
+
+export interface UserManagerResponse {
+  message: string
+  success: boolean
+  user: UserManager
+}
