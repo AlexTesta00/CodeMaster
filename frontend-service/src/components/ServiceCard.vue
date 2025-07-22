d5dae41dd0c191e3aeab5c796fb66edac4764d9b
Alex Testa <alex.testa@studio.unibo.it>
2025-07-22 17:19:42 +0200

    feat(frontend): added /monitor routes for check service status

---
+++ frontend-service/src/components/ServiceCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+  name: String,
+  status: Boolean,
+})
+</script>
+
+<template>
+  <div class="bg-secondary w-4/5 h-32 lg:w-1/4 lg:h-44 mt-8 rounded-3xl flex flex-col items-start justify-center gap-4 lg:ml-6">
+    <h1 class="text-4xl ml-7">{{ name }}</h1>
+    <p class="ml-7 "> Status:
+      <span v-if="status"> Online
+        <span class="animate-pulse">🟢</span>
+      </span> <span v-else> Offline
+        <span class="animate-pulse">🔴</span>
+      </span>
+    </p>
+  </div>
+</template>
