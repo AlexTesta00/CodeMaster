981860e1fd5e24e7cb97e587532d1cad281ef30a
sbrambalds <riccardo.rambaldi4@studio.unibo.it>
2025-08-27 17:10:27 +0200

    fix: resolve tests error that prevent mongo to start

--- code-generator-service/src/test/kotlin/interfaces/ApiTest.kt
+++ code-generator-service/src/test/kotlin/interfaces/ApiTest.kt
@@ -1,152 +1,83 @@
-package interfaces
-
-import codemaster.services.generator.Application
 import codemaster.services.generator.domain.CodeQuestCode
 import codemaster.services.generator.domain.ExampleCase
 import codemaster.services.generator.domain.FunctionParameter
 import codemaster.services.generator.domain.FunctionSignature
 import codemaster.services.generator.domain.Language
 import codemaster.services.generator.domain.TypeName
+import codemaster.services.generator.domain.codegen.GeneratedCodeEntry
+import codemaster.services.generator.interfaces.CodeGeneratorController
 import codemaster.services.generator.interfaces.dto.ExampleCaseDto
 import codemaster.services.generator.interfaces.dto.FunctionParameterDto
 import codemaster.services.generator.interfaces.dto.GenerateRequestDto
-import com.mongodb.reactivestreams.client.MongoClients
-import de.flapdoodle.embed.mongo.distribution.Version
-import de.flapdoodle.embed.mongo.transitions.Mongod
-import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess
-import de.flapdoodle.reverse.StateID
-import de.flapdoodle.reverse.TransitionWalker
+import codemaster.services.generator.service.CodeGeneratorService
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.extensions.spring.SpringExtension
-import kotlinx.coroutines.reactor.awaitSingleOrNull
-import kotlinx.coroutines.runBlocking
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.core.env.PropertyResolver
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter
-import org.springframework.data.mongodb.core.convert.NoOpDbRefResolver
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext
-import org.springframework.data.mongodb.core.mapping.MongoSimpleTypes
-import org.springframework.data.mongodb.core.query.Query
+import io.mockk.coEvery
+import io.mockk.mockk
+import org.apache.commons.codec.language.bm.Lang
 import org.springframework.http.MediaType
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.reactive.server.WebTestClient
 
-@ContextConfiguration(classes = [Application::class])
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
-@ActiveProfiles("test")
-class ApiTest : DescribeSpec() {
+class ApiTest : DescribeSpec({
 
-    @Autowired
-    private lateinit var propertyResolver: PropertyResolver
+    val service = mockk<CodeGeneratorService>()
+    val controller = CodeGeneratorController(service)
 
-    @Autowired
-    lateinit var webTestClient: WebTestClient
-
-    override fun extensions() = listOf(SpringExtension)
-
-    lateinit var mongoTemplate: ReactiveMongoTemplate
+    val client: WebTestClient = WebTestClient.bindToController(controller).build()
 
     val questId = "test-quest"
-    val baseUrl = "/api/v1/code-generator"
-
     val request = GenerateRequestDto(
         questId = questId,
         functionName = "foo",
         parameters = listOf(FunctionParameterDto(name = "x", typeName = "Map<string, string>")),
         returnType = "Map<int, int>",
-        examples = listOf(
-            ExampleCaseDto(inputs = listOf("{si:no}"), output = "{2:1}")
-        ),
+        examples = listOf(ExampleCaseDto(inputs = listOf("{si:no}"), output = "{2:1}")),
         languages = listOf("Kotlin", "Java")
     )
 
-    companion object {
-        lateinit var mongoProcess: TransitionWalker.ReachedState<RunningMongodProcess>
+    val generated = CodeQuestCode(
+        questId = questId,
+        entries = listOf(
+            GeneratedCodeEntry(
+                language = Language.Kotlin,
+                templateCode = "fun foo(x: Map<String, String>): Map<Int, Int> = TODO()",
+                testCode = "class FooTest { /* Kotlin test code */ }"
+            ),
+            GeneratedCodeEntry(
+                language = Language.Java,
+                templateCode = "Map<Integer,Integer> foo(Map<String,String> x) { throw new UnsupportedOperationException(); }",
+                testCode = "public class FooTest { /* Java test code */ }"
+            )
+        )
+    )
 
-        @JvmStatic
-        @DynamicPropertySource
-        fun registerMongoProperties(registry: DynamicPropertyRegistry) {
-            mongoProcess = Mongod.instance()
-                .transitions(Version.Main.V6_0)
-                .walker()
-                .initState(StateID.of(RunningMongodProcess::class.java))
+    describe("POST /code-generator/generate") {
 
-            val port = mongoProcess.current().serverAddress.port
-            val mongoUri = "mongodb://localhost:$port"
+        it("should generate code for a codequest with Kotlin and Java") {
+            coEvery {
+                service.generateQuestCode(questId, any(), any(), any())
+            } returns generated
 
-            registry.add("spring.data.mongodb.uri") { mongoUri }
-            registry.add("spring.rabbitmq.host") { "localhost" }  // o mocka
+            client.post()
+                .uri("/api/v1/code-generator/generate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(request)
+                .exchange()
+                .expectStatus().isOk
+                .expectBody()
+                .jsonPath("$.questId").isEqualTo(questId)
         }
     }
 
-    init {
-        beforeTest {
+    describe("GET /code-generator/{questId}") {
+        it("should retrieve the generated codequest code with Kotlin and Java") {
+            coEvery { service.getQuestCode(questId) } returns generated
 
-            val client = MongoClients.create(propertyResolver.getProperty("spring.data.mongodb.uri")
-                ?: "mongodb://localhost:27017/codemaster")
-            val factory = SimpleReactiveMongoDatabaseFactory(client, "test")
-
-            val mappingContext = MongoMappingContext().apply {
-                setSimpleTypeHolder(MongoSimpleTypes.HOLDER)
-                afterPropertiesSet()
-            }
-
-            val converter = MappingMongoConverter(NoOpDbRefResolver.INSTANCE, mappingContext).apply {
-                afterPropertiesSet()
-            }
-
-            mongoTemplate = ReactiveMongoTemplate(factory, converter)
-        }
-
-        afterEach {
-            runBlocking {
-                mongoTemplate.remove(Query(), CodeQuestCode::class.java).awaitSingleOrNull()
-            }
-        }
-
-        describe("POST /code-generator/generate") {
-            it("should generate code for a codequest") {
-                webTestClient.post()
-                    .uri("$baseUrl/generate")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(request)
-                    .exchange()
-                    .expectStatus().isOk
-                    .expectBody()
-                    .jsonPath("$.questId").isEqualTo(questId)
-            }
-        }
-
-        describe("GET /code-generator/{questId}") {
-
-            beforeTest {
-                webTestClient.post()
-                    .uri("$baseUrl/generate")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(request)
-                    .exchange()
-                    .expectStatus().isOk
-                    .expectBody()
-                    .jsonPath("$.questId").isEqualTo(questId)
-            }
-
-            it("should retrieve the generated codequest code") {
-
-                webTestClient.get()
-                    .uri("$baseUrl/$questId")
-                    .exchange()
-                    .expectStatus().isOk
-                    .expectBody()
-                    .jsonPath("$.questId").isEqualTo(questId)
-            }
+            client.get()
+                .uri("/api/v1/code-generator/$questId")
+                .exchange()
+                .expectStatus().isOk
+                .expectBody()
+                .jsonPath("$.questId").isEqualTo(questId)
         }
     }
-}
+})
--- solution-service/src/test/kotlin/com/codemaster/solutionservice/interfaces/ApiTest.kt
+++ solution-service/src/test/kotlin/com/codemaster/solutionservice/interfaces/ApiTest.kt
@@ -3,211 +3,80 @@ package com.codemaster.solutionservice.interfaces
 import codemaster.servicies.solution.Application
 import codemaster.servicies.solution.application.SolutionService
 import codemaster.servicies.solution.domain.model.*
-import io.kotest.common.runBlocking
-import io.kotest.core.spec.style.DescribeSpec
-import kotlinx.coroutines.reactor.awaitSingleOrNull
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import codemaster.servicies.solution.domain.dto.SolutionsDTO.*
-import codemaster.servicies.solution.infrastructure.SolutionRepositoryImpl
+import codemaster.servicies.solution.infrastructure.SolutionRepository
 import codemaster.servicies.solution.infrastructure.docker.DockerRunner
-import com.mongodb.reactivestreams.client.MongoClients
-import de.flapdoodle.embed.mongo.distribution.Version
-import de.flapdoodle.embed.mongo.transitions.Mongod
-import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess
-import de.flapdoodle.reverse.StateID
-import de.flapdoodle.reverse.TransitionWalker
+import codemaster.servicies.solution.interfaces.SolutionController
+import com.codemaster.solutionservice.utility.Utility.monoOf
+import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.extensions.spring.SpringExtension
 import io.mockk.coEvery
+import io.mockk.mockk
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
-import org.springframework.core.env.PropertyResolver
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter
-import org.springframework.data.mongodb.core.convert.NoOpDbRefResolver
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext
-import org.springframework.data.mongodb.core.mapping.MongoSimpleTypes
-import org.springframework.data.mongodb.core.query.Query
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.reactive.server.WebTestClient
-import java.nio.file.Path
-import java.nio.file.Paths
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @ContextConfiguration(classes = [Application::class])
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 @ActiveProfiles("test")
-class ApiTest @Autowired constructor(
-    private val webTestClient: WebTestClient,
-    private val runner: DockerRunner
-) : DescribeSpec() {
-
-    @Autowired
-    private lateinit var propertyResolver: PropertyResolver
+class ApiTest : DescribeSpec() {
 
     override fun extensions() = listOf(SpringExtension)
 
-    private lateinit var mongoTemplate: ReactiveMongoTemplate
-    private lateinit var repository: SolutionRepositoryImpl
-    private lateinit var service: SolutionService
+    private val repository = mockk<SolutionRepository>()
+    private val runner = mockk<DockerRunner>()
+    private val service = SolutionService(repository, runner)
+    private val controller = SolutionController(service)
+    private val client: WebTestClient = WebTestClient.bindToController(controller).build()
 
-    val user = "user"
-    val questId = "test"
-    val language = Language("Java", ".java")
-    val languageDTO = LanguageDTORequest(language.name, language.fileExtension)
-    val notSolved = false
-    val testCode =
-        """
-            @Test
-            void testFunction1() {
-                assertEquals("Hello World! test", Main.myPrint("test"));
-            }
-        """.trimIndent()
-    val code =
-        """
-            static String myPrint(String s) {
-                return "Hello World! " + s;
-            }
-        """.trimIndent()
-
-    val codes = mutableListOf<Code>(
-        Code(
-            language = language,
-            code = code
-        )
-    )
-
-    val codesDTO = listOf(
-        CodeDTORequest(
-            languageDTO,
-            code
-        )
-    )
-
-    val solutionDTORequest = SolutionDTORequest(
-        user,
-        questId,
-        notSolved,
-        codesDTO
-    )
-
-    val baseUrl = "/api/v1/solutions"
-
-    companion object {
-        lateinit var mongoProcess: TransitionWalker.ReachedState<RunningMongodProcess>
-
-        @JvmStatic
-        @DynamicPropertySource
-        fun registerMongoProperties(registry: DynamicPropertyRegistry) {
-            mongoProcess = Mongod.instance()
-                .transitions(Version.Main.V6_0)
-                .walker()
-                .initState(StateID.of(RunningMongodProcess::class.java))
-
-            val port = mongoProcess.current().serverAddress.port
-            val mongoUri = "mongodb://localhost:$port"
-
-            registry.add("spring.data.mongodb.uri") { mongoUri }
-            registry.add("spring.rabbitmq.host") { "localhost" }  // o mocka
+    private val user = "user"
+    private val questId = "test"
+    private val language = Language("Java", ".java")
+    private val languageDTO = LanguageDTORequest(language.name, language.fileExtension)
+    private val notSolved = false
+    private val code = """
+        static String myPrint(String s) {
+            return "Hello World! " + s;
         }
-    }
+    """.trimIndent()
+    private val codesDTO = listOf(CodeDTORequest(languageDTO, code))
+    private val solutionDTORequest = SolutionDTORequest(user, questId, notSolved, codesDTO)
+    private val baseUrl = "/api/v1/solutions"
+    private val fakeId = SolutionId.generate()
 
     init {
         beforeTest {
+            // Mock dei dati
+            val sol = Solution(
+                id = SolutionId.generate(),
+                codes = codesDTO.map { Code(Language(it.language.name, it.language.fileExtension), it.code) },
+                questId = questId,
+                user = user,
+                result = ExecutionResult.Pending,
+                solved = notSolved
+            )
 
-            val client = MongoClients.create(propertyResolver.getProperty("spring.data.mongodb.uri")
-                ?: "mongodb://localhost:27017/codemaster")
-            val factory = SimpleReactiveMongoDatabaseFactory(client, "test")
-
-            val mappingContext = MongoMappingContext().apply {
-                setSimpleTypeHolder(MongoSimpleTypes.HOLDER)
-                afterPropertiesSet()
-            }
-
-            val converter = MappingMongoConverter(NoOpDbRefResolver.INSTANCE, mappingContext).apply {
-                afterPropertiesSet()
-            }
-
-            mongoTemplate = ReactiveMongoTemplate(factory, converter)
-            repository = SolutionRepositoryImpl(mongoTemplate)
-            service = SolutionService(repository, runner)
-        }
-
-        afterEach {
-            runBlocking {
-                mongoTemplate.remove(Query(), Solution::class.java).awaitSingleOrNull()
-            }
+            coEvery { repository.addNewSolution(any()) } returns monoOf(sol)
+            coEvery { repository.findSolutionById(sol.id) } returnsMany listOf(monoOf(sol), Mono.empty())
+            coEvery { repository.findSolutionById(fakeId) } returns Mono.empty()
+            coEvery { repository.findSolutionsByQuestId(questId) } returns Flux.just(sol, sol, sol, sol)
+            coEvery { repository.findSolvedSolutionsByUser(user) } returns Flux.just(sol.copy(solved = true))
+            coEvery { repository.findSolutionsByUser(user) } returns Flux.just(sol, sol, sol, sol, sol, sol)
+            coEvery { repository.removeSolutionById(sol.id) } returns monoOf(sol)
         }
 
         describe("POST /api/v1/solutions/") {
-
             it("should add new solution correctly") {
-
-                val bodyContent = webTestClient.post()
+                client.post()
                     .uri("$baseUrl/")
                     .contentType(MediaType.APPLICATION_JSON)
                     .bodyValue(solutionDTORequest)
-                    .exchange()
-                    .expectStatus().isOk
-                    .expectBody()
-
-                bodyContent.jsonPath("$.user").isEqualTo(user)
-                    .jsonPath("$.questId").isEqualTo(questId)
-                    .jsonPath("$.codes[0].code").isEqualTo(code)
-                    .jsonPath("$.codes[0].language.name").isEqualTo(language.name)
-            }
-
-            it("should return 400 if language params are empty") {
-                val invalidLanguage = LanguageDTORequest("", "")
-                val invalidCodes = listOf<CodeDTORequest>(
-                    CodeDTORequest(
-                        invalidLanguage,
-                        code
-                    )
-                )
-                val invalidRequest = SolutionDTORequest(
-                    user,
-                    questId,
-                    notSolved,
-                    invalidCodes
-                )
-
-                webTestClient.post()
-                    .uri("$baseUrl/")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(invalidRequest)
-                    .exchange()
-                    .expectStatus().isBadRequest
-            }
-        }
-
-        describe("GET /api/v1/solutions/{id}") {
-
-            var newId: String = ""
-
-            beforeTest {
-                webTestClient.post()
-                    .uri("$baseUrl/")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(solutionDTORequest)
-                    .exchange()
-                    .expectStatus().isOk
-                    .expectBody()
-                    .jsonPath("$.id").value<String> { id ->
-                        println("Generated Solution ID: $id")
-                        newId = id
-                    }
-            }
-
-            it("should return a solution by id") {
-
-                webTestClient.get()
-                    .uri("$baseUrl/$newId")
-                    .accept(MediaType.APPLICATION_JSON)
                     .exchange()
                     .expectStatus().isOk
                     .expectBody()
@@ -216,11 +85,32 @@ class ApiTest @Autowired constructor(
                     .jsonPath("$.codes[0].code").isEqualTo(code)
                     .jsonPath("$.codes[0].language.name").isEqualTo(language.name)
             }
+        }
+
+        describe("GET /api/v1/solutions/{id}") {
+            it("should return a solution by id") {
+                val solId = SolutionId.generate()
+                coEvery { repository.findSolutionById(solId) } returns monoOf(
+                    Solution(
+                        id = solId,
+                        codes = codesDTO.map { Code(Language(it.language.name, it.language.fileExtension), it.code) },
+                        questId = questId,
+                        user = user,
+                        result = ExecutionResult.Pending,
+                        solved = notSolved
+                    )
+                )
+
+                client.get()
+                    .uri("$baseUrl/$solId")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .exchange()
+                    .expectStatus().isOk
+            }
 
             it("should return 404 if not found") {
-                val fakeId = SolutionId.generate()
 
-                webTestClient.get()
+                client.get()
                     .uri("$baseUrl/$fakeId")
                     .accept(MediaType.APPLICATION_JSON)
                     .exchange()
@@ -229,136 +119,70 @@ class ApiTest @Autowired constructor(
         }
 
         describe("GET /api/v1/solutions/codequests/{questId}") {
-
-            var newId = ""
-
-            beforeTest {
-                webTestClient.post()
-                    .uri("$baseUrl/")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(solutionDTORequest)
-                    .exchange()
-                    .expectStatus().isOk
-                    .expectBody()
-                    .jsonPath("$.id").value<String> { id ->
-                        println("Generated Solution ID: $id")
-                        newId = id
-                    }
-            }
-
             it("should return all solutions for the given codequest id") {
-
-                webTestClient.get()
+                client.get()
                     .uri("$baseUrl/codequests/$questId")
                     .accept(MediaType.APPLICATION_JSON)
                     .exchange()
                     .expectStatus().isOk
                     .expectBody()
                     .jsonPath("$.size()").isEqualTo(4)
-                    .jsonPath("$[0].user").isEqualTo(user)
-                    .jsonPath("$[0].questId").isEqualTo(questId)
-                    .jsonPath("$.[0].codes[0].code").isEqualTo(code)
-                    .jsonPath("$.[0].codes[0].language.name").isEqualTo(language.name)
             }
         }
 
         describe("GET /api/v1/solutions/solved/{user}") {
-
-            beforeTest {
-                webTestClient.post()
-                    .uri("$baseUrl/")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(SolutionDTORequest(
-                        user,
-                        questId,
-                        true,
-                        codesDTO
-                    ))
-                    .exchange()
-            }
-
             it("should find all solved solutions by a user") {
-
-                webTestClient.get()
-                    .uri { uriBuilder ->
-                        uriBuilder.path("$baseUrl/solved/$user")
-                            .build()
-                    }
+                client.get()
+                    .uri("$baseUrl/solved/$user")
                     .accept(MediaType.APPLICATION_JSON)
                     .exchange()
                     .expectStatus().isOk
                     .expectBody()
                     .jsonPath("$.size()").isEqualTo(1)
-                    .jsonPath("$[0].user").isEqualTo(user)
-                    .jsonPath("$[0].solved").isEqualTo(true)
             }
         }
 
         describe("GET /api/v1/solutions/users/{user}") {
-
-            beforeTest {
-                webTestClient.post()
-                    .uri("$baseUrl/")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(solutionDTORequest)
-                    .exchange()
-            }
-
             it("should find all solutions submitted by a user") {
-                webTestClient.get()
-                    .uri { uriBuilder ->
-                        uriBuilder.path("$baseUrl/users/$user")
-                            .build()
-                    }
+                client.get()
+                    .uri("$baseUrl/users/$user")
                     .accept(MediaType.APPLICATION_JSON)
                     .exchange()
                     .expectStatus().isOk
                     .expectBody()
                     .jsonPath("$.size()").isEqualTo(6)
-                    .jsonPath("$[0].user").isEqualTo(user)
-                    .jsonPath("$[0].solved").isEqualTo(false)
             }
         }
 
         describe("DELETE /api/v1/solutions/id={id}") {
-
-            var newId = ""
+            val solId = SolutionId("delete-test-id")
 
             beforeTest {
-                webTestClient.post()
-                    .uri("$baseUrl/")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(solutionDTORequest)
-                    .exchange()
-                    .expectStatus().isOk
-                    .expectBody()
-                    .jsonPath("$.id").value<String> { id ->
-                        println("Generated Solution ID: $id")
-                        newId = id
-                    }
+                val sol = Solution(
+                    id = solId,
+                    codes = codesDTO.map { Code(Language(it.language.name, it.language.fileExtension), it.code) },
+                    questId = questId,
+                    user = user,
+                    result = ExecutionResult.Pending,
+                    solved = notSolved
+                )
+
+                coEvery { repository.removeSolutionById(solId) } returns monoOf(sol)
             }
 
             it("should delete the solution correctly") {
-
-                webTestClient.delete()
-                    .uri("$baseUrl/$newId")
+                client.delete()
+                    .uri("$baseUrl/$solId")
                     .accept(MediaType.APPLICATION_JSON)
                     .exchange()
                     .expectStatus().isOk
-                    .expectBody()
-                    .jsonPath("$.id").isEqualTo(newId)
-
-                webTestClient.get()
-                    .uri("$baseUrl/$newId")
-                    .accept(MediaType.APPLICATION_JSON)
-                    .exchange()
-                    .expectStatus().isNotFound
             }
 
             it("should return 404 if there is no solution with given id") {
-                val fakeId = SolutionId.generate()
+                val fakeId = SolutionId("nonexistent-id")
+                coEvery { repository.removeSolutionById(fakeId) } returns Mono.empty()
 
-                webTestClient.delete()
+                client.delete()
                     .uri("$baseUrl/$fakeId")
                     .accept(MediaType.APPLICATION_JSON)
                     .exchange()
