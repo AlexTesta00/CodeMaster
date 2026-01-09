01e1e19cc7b8fa93f072088b93dc10220b1212c8
sbrambalds <riccardo.rambaldi4@studio.unibo.it>
2026-01-09 14:42:19 +0100

    refactor(docker): split docker compose into multiple file

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,12 @@ fun dockerCmd(): Any {
 
 tasks.register<Exec>("dockerCompose") {
     println("Docker build completed")
-    commandLine(dockerCmd(), "compose", "up", "--build")
+    commandLine(dockerCmd(), "compose",
+        "-f", "docker-compose.net.yml",
+        "-f", "docker-compose.mongo.yml",
+        "-f", "docker-compose.services.yml",
+        "-f", "docker-compose.docs.yml",
+        "up", "--build")
 
     dependsOn("buildMultiLangRunner")
 }
